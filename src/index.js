78670098f879e77fd8bf07802bf207781bab8dc4
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2023-02-08 14:53:06 +0800

    wrap in async

--- src/index.js
+++ src/index.js
@@ -4,22 +4,26 @@ const resolvers = require("./resolvers");
 
 const TrackAPI = require("./datasources/track-api");
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI(),
-    };
-  },
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
 
-const PORT = process.env.PORT || 4000;
+  const PORT = process.env.PORT || 4000;
 
-await server.listen({ port: PORT }).then(() => {
-  console.log(`
+  await server.listen({ port: PORT }).then(() => {
+    console.log(`
     🚀  Server is running!
     🔉  Listening on port ${PORT}
     📭  Query at http://localhost:${PORT}
   `);
-});
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
